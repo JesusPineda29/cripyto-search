b087a44952062d4b995f594744f5496bf16d6327
JesusPineda29 <jesuspineda1429@gmail.com>
2025-06-12 12:30:09 -0600

    agregando el componente del formulario

--- src/components/CriptoSearchForm.tsx
+++ src/components/CriptoSearchForm.tsx
@@ -2,7 +2,23 @@
 
 
 export const CriptoSearchForm = () => {
-  return (
-    <div>CriptoSearchForm</div>
-  )
+    return (
+        <form className="form">
+            <div className="field">
+                <label htmlFor="currency">Moneda:</label>
+                <select name="currency" id="currency">
+                    <option value="">-- Seleccione --</option>
+                </select>
+            </div>
+
+            <div className="field">
+                <label htmlFor="criptocurrency">Criptooneda:</label>
+                <select name="criptocurrency" id="criptocurrency">
+                    <option value="">-- Seleccione --</option>
+                </select>
+            </div>
+
+            <input type="submit" value='Cotizar' />
+        </form>
+    )
 }
